16ef8fcc88fa4f255f8e730ba5a620ce934e78b5
skoruole <skoruole@cvut.cz>
2023-04-11 00:15:18 +0200

    Listening

--- server.cpp
+++ server.cpp
@@ -8,15 +8,69 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char **argv){
+constexpr int TIMEOUT = 10;
+constexpr int BUFFER_SIZE = 1024;
 
-    if(argc < 2){
-        return -1;
+int main(int argc, char **argv){
+    int server, new_client_socket, port;
+    char buffer[BUFFER_SIZE] = {0};
+    if(argc < 2){ return cerr << "Usage: server port" << endl , -1;}
+
+     // Создание сокета
+    if ((server = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
     }
 
-    while(true){
+    port = atoi(argv[1]);
+    if(port == 0){
+        cerr << "Usage: server port" << endl;
+        close(server);
+        return -1;
+    }
+   
+    struct sockaddr_in address;
+    bzero(&address, sizeof(address));
+    address.sin_family = AF_INET;
+    address.sin_port = htons(port);
+    address.sin_addr.s_addr = htonl(INADDR_ANY);
+    int addrlen = sizeof(address);
+
+    if (bind(server, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Прослушивание порта
+    if (listen(server, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
 
 
+     while (1) {
+        if ((new_client_socket = accept(server, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+
+        // Обработка подключений с помощью fork()
+        if (fork() == 0) {
+            close(server); // Закрыть родительский сокет в дочернем процессе
+
+            // Чтение и вывод сообщений
+            int read_bytes;
+            while ((read_bytes = read(new_client_socket, buffer, BUFFER_SIZE)) > 0) {
+                buffer[read_bytes] = '\0';
+                std::cout << "Received message: " << buffer << std::endl;
+            }
+
+            close(new_client_socket); // Закрыть дочерний сокет
+            exit(EXIT_SUCCESS);
+        } else {
+            close(new_client_socket); // Закрыть дочерний сокет в родительском процессе
+            waitpid(-1, nullptr, WNOHANG); // Очистка завершившихся дочерних процессов
+        }
     }
     return 0;
 }
